42bcc6b0747f7a907b49d55adf85450ebad6c93d
RyZel0l <jacecalhoun816@gmail.com>
2025-05-27 08:07:27 -0400

    first commit

---
+++ src/main/java/net/cash/tutorial/KunpenjoeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cash.tutorial;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KunpenjoeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/cash/tutorial/KupenjoeClient.java
@@ -0,0 +1,10 @@
+package net.cash.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KupenjoeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
