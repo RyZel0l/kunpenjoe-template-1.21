4c8cf618d279382c016b3a8926829bba2bc73c39
RyZel0l <jacecalhoun816@gmail.com>
2025-05-27 08:08:59 -0400

    added important comment

--- src/main/java/net/cash/tutorial/Kunpenjoe.java
+++ src/main/java/net/cash/tutorial/Kunpenjoe.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// Very important comment
 public class Kunpenjoe implements ModInitializer {
 	public static final String MOD_ID = "kunpenjoe";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
